7a3027f00b806a27e7e01b9c83f7f9a36685df22
amandadeog <amandadeog@gmail.com>
2023-05-01 10:49:12 -0300

    cria a seção depoimentos

---
+++ src/components/Carrossel/index.tsx
@@ -0,0 +1,70 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Carrossel.css'
+import CardDepoimentos from '../CardDepoimentos';
+
+
+const depoiments = [
+   {
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione reprehenderit accusamus rerum voluptas at. Cupiditate odit earum ex modi reiciendis dolore molestias quia recusandae eveniet totam, perspiciatis temporibus. Quia, modi.',
+      image: 'https://github.com/amandadeogr.png',
+      alt: 'imagem amanda',
+      author: 'Amanda Ramirez',
+   },
+   {
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione reprehenderit accusamus rerum voluptas at. Cupiditate odit earum ex modi reiciendis dolore molestias quia recusandae eveniet totam, perspiciatis temporibus. Quia, modi.',
+      image: 'https://github.com/ramirezmz.png',
+      alt: 'imagem amanda',
+      author: 'Roberto Ramirez',
+   },
+   {
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione reprehenderit accusamus rerum voluptas at. Cupiditate odit earum ex modi reiciendis dolore molestias quia recusandae eveniet totam, perspiciatis temporibus. Quia, modi.',
+      image: 'https://github.com/amandadeogr.png',
+      alt: 'imagem amanda',
+      author: 'Amanda Ramirez',
+   },
+   {
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione reprehenderit accusamus rerum voluptas at. Cupiditate odit earum ex modi reiciendis dolore molestias quia recusandae eveniet totam, perspiciatis temporibus. Quia, modi.',
+      image: 'https://github.com/ramirezmz.png',
+      alt: 'imagem amanda',
+      author: 'Roberto Ramirez',
+   },
+   {
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione reprehenderit accusamus rerum voluptas at. Cupiditate odit earum ex modi reiciendis dolore molestias quia recusandae eveniet totam, perspiciatis temporibus. Quia, modi.',
+      image: 'https://github.com/amandadeogr.png',
+      alt: 'imagem amanda',
+      author: 'Amanda Ramirez',
+   },
+   {
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione reprehenderit accusamus rerum voluptas at. Cupiditate odit earum ex modi reiciendis dolore molestias quia recusandae eveniet totam, perspiciatis temporibus. Quia, modi.',
+      image: 'https://github.com/ramirezmz.png',
+      alt: 'imagem amanda',
+      author: 'Roberto Ramirez',
+   },
+   
+]
+
+const Carrossel = () => {
+   const settings = {
+     dots: true,
+     infinite: false,
+     speed: 500,
+     slidesToShow: 2,
+     slidesToScroll: 1,
+   };
+ 
+   return (
+     <Slider {...settings}>
+       {
+         depoiments.map( (item) => {
+            return (
+               <CardDepoimentos description={item.description} image={item.image} alt={item.alt} author={item.author}/>
+            )
+         })
+      }
+     </Slider>
+   );
+ }
+ 
+ export default Carrossel;
---
+++ src/components/Depoimentos/index.tsx
@@ -0,0 +1,14 @@
+import Carrossel from '../Carrossel';
+import './Depoimentos.css';
+
+
+const Depoimentos = () => {
+   return (
+      <section className='depoiments'>
+         <h2 className='depoiments-title'>DEPOIMENTOS</h2>
+         <Carrossel />
+      </section>
+   )
+}
+
+export default Depoimentos;
