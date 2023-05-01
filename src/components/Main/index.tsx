0dd746fca3910a71ebac024569f08451c48979b3
amandadeog <amandadeog@gmail.com>
2023-05-01 10:50:25 -0300

    cria o componente Main

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import Banner from "../Banner"
+import Depoimentos from "../Depoimentos";
+import Services from "../Services";
+
+const Main = () => {
+   return (
+      <>
+         <Banner />
+         <Services />
+         <Depoimentos />
+      </>
+   )
+}
+
+export default Main;
