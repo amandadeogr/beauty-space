fe848af1a043b6a5eef55c9d8d86d4064cce394f
amandadeog <amandadeog@gmail.com>
2023-05-01 10:46:21 -0300

    cria a seção Banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,20 @@
+import './Banner.css';
+import modelo from '../../assets/modelo.png';
+import Button from '../Button';
+
+const Banner = () => {
+   return (
+      <section className="banner">
+         <div className="right">
+            <h1 className='banner-title'>MARIA</h1>
+            <p className='banner-description'>O Espaço de Beleza mais completo do Brasil, onde você encontra os melhores tratamentos e técnicas de beleza, feitas pelos profissionais mais renomados do país, prontos para te atender.</p>
+            <Button />
+         </div>
+
+         <img className='banner-img' src={modelo} alt="Modelo de pele branca, olhos claros, e cabelos claros e ondulados" />
+
+      </section>
+   )
+}
+
+export default Banner;
