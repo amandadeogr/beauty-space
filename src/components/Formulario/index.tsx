507d8683f612275a793b361a8c4543c679fff838
amandadeog <amandadeog@gmail.com>
2023-05-05 19:13:31 -0300

    cria componente de feedback para validação do formulário

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,9 @@
 import "./Formulario.css";
 import Button from "../Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
 import Input from "../Input";
+import Feedback from "../Feedback";
 
 const KEY = import.meta.env;
 
@@ -10,28 +11,42 @@ const Formulario = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [feedback, setFeedback] = useState<{ message: string, messageType: 'success' | 'warning' | 'error' }>();
+
+  useEffect(() => {
+    if (feedback) {
+      const timer = setTimeout(() => {
+        setFeedback(undefined);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [feedback]);
 
   const sendEmail = (e: any) => {
     e.preventDefault();
-    if(name === '' || email === '' || message === '') {
-       alert('preencha todos os campos')
-       return
-    }
-       const templateParams = {
-          from_name: name,
-          message: message,
-          email: email
-       }
 
-       emailjs.send(KEY.VITE_API_SERVICE_KEY, KEY.VITE_API_TEMPLATE_KEY, templateParams, KEY.VITE_PUBLIC_KEY)
-       .then((response) => {
-          console.log('Email enviado', response.status, response.text);
-          setName('');
-          setEmail('');
-          setMessage('');
-       }, (err) => {
-          console.log('Error: ', err);
-       })
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+   }
+
+    if(name === '' || email === '' || message === '') {
+      setFeedback({ message: 'O preenchimento de todos os campos é requerido', messageType: 'warning' });
+       return
+    }      
+
+    {
+      emailjs.send(KEY.VITE_API_SERVICE_KEY, KEY.VITE_API_TEMPLATE_KEY, templateParams, KEY.VITE_PUBLIC_KEY)
+      .then((response) => {
+       setFeedback({ message: 'Mensagem enviada com sucesso!', messageType: 'success' });
+         setName('');
+         setEmail('');
+         setMessage('');
+      }, (err) => {
+       setFeedback({ message: 'Errr' + err, messageType: 'error' });
+      })
+    }
   };
 
   return (
@@ -58,6 +73,10 @@ const Formulario = () => {
           name={message}
           onChange={(e) => setMessage(e.target.value)}
         />
+        
+        {
+          feedback && <Feedback message={feedback.message} messageType={feedback.messageType}/>
+        }
 
         <Button action="email" message="Enviar mensagem" />
       </form>
