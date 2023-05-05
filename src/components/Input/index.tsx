0bf47edc3a589c1c3901bafd36873c8b0fd23a0f
amandadeog <amandadeog@gmail.com>
2023-05-04 22:48:29 -0300

    instala emailjs e devidas configurações

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,11 +3,13 @@ import './Input.css';
 interface Props {
    placeholder: string,
    type: string,
+   value: string,
+   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({placeholder, type}: Props) => {
+const Input = ({placeholder, type, value, onChange}: Props) => {
   return (
-    <input className="input" type={type} placeholder={placeholder}/>
+    <input className="input" type={type} value={value} placeholder={placeholder} onChange={onChange}/>
   )
 }
 
